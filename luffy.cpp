ef25b0eec5bc39fa094c264c3065504ce1c25efc
yazansharba <141403974+yazansharba@users.noreply.github.com>
2025-09-12 13:24:02 +0300

    add

---
+++ luffy.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define pb push_back
+#define all(a) a.begin(),a.end()
+const int LOG=31;
+const int mxn=2e5+5;
+const ll inf =1e18;
+const int mod=998244353;
+
+void solve(){
+}
+signed main(){
+    ios_base::sync_with_stdio(NULL);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+#ifdef Usaco
+    string f="shortcut";
+    freopen((f+".in").c_str(),"r",stdin);
+    freopen((f+".out").c_str(),"w",stdout);
+#endif
+    ll tt = 1;
+    cin >> tt;
+    while (tt--){
+        solve();
+        cout<<"\n";
+    }
+}
